099b57879976ef9577e1ab5f58d9113855b5ddd2
Ratikanta Rout <ratikantrout171@gmail.com>
2023-10-02 13:40:55 +0530

    CheckPass-2-Bug1

--- src/searchenginepy/__init__.py
+++ src/searchenginepy/__init__.py
@@ -1,37 +1,40 @@
 # __init__.py
-# #importing google class
+import logging
 from .Google import Google
 from .Bing import Bing
 from .DuckDuckGo import Duckduckgo
 from .Brave import Brave
-import logging
-__pkgname='searchenginepy'
+
+__pkgname = 'searchenginepy'
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
-
-log.info('Initializing '+__pkgname+' package')
+log.info('Initializing ' + __pkgname + ' package')
 
 class SearchEngine:
-    def __init__(self, query,pagenumber=1):
+    def __init__(self, query, pagenumber=1):
         self.query = query
-        self.results = []
-        self.pagenumber=pagenumber
+        self.pagenumber = pagenumber
+
     def google(self):
-        googleobj=Google()
-        result=googleobj.search(self.query,self.pagenumber)
+        google_obj = Google()
+        result = google_obj.search(self.query, self.pagenumber)
         return result
+
     def bing(self):
-        bingobj=Bing()
-        result=bingobj.search(self.query,self.pagenumber)
+        bing_obj = Bing()
+        result = bing_obj.search(self.query, self.pagenumber)
         return result
+
     def duckduckgo(self):
-        duckduckgoobj=Duckduckgo()
-        result=duckduckgoobj.search(self.query,self.pagenumber)
+        duckduckgo_obj = Duckduckgo()
+        result = duckduckgo_obj.search(self.query, self.pagenumber)
         return result
+
     def brave(self):
-        braveobj=Brave()
-        result=braveobj.search(self.query,self.pagenumber)
+        brave_obj = Brave()
+        result = brave_obj.search(self.query, self.pagenumber)
         return result
+
     def get_results(self):
         return self.results
 
